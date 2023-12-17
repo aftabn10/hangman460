65817f1fa03e4c1aacd07365d9b828c9f7a296de
Aftab Najib <aftab@mediadezines.co.uk>
2023-12-17 20:58:26 +0000

    Add check_guess, ask_for_input functions

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -16,22 +16,53 @@ print(type(word))
 
 single_word = list(word)
 
-while True:
-    guess = input("Please guess the letter: ")
+# # original code I wrote 
+# while True:
+#     guess = input("Please guess the letter: ")
     
-    if len(guess) == 1 and guess.isalpha():
-        # Added flag in order to move onto the next set of if statements, other was exiting due to break
-        valid_guess = True
-        break
-    else:
-        print("Invalid letter. Please enter a single alphabetical character.") 
+#     if len(guess) == 1 and guess.isalpha():
+#         # Added flag in order to move onto the next set of if statements, other was exiting due to break
+#         valid_guess = True
+#         break
+#     else:
+#         print("Invalid letter. Please enter a single alphabetical character.") 
+# #original code stop
 
 #Now check if the user has guessed the 'right' letter from the word 
-if valid_guess:   
-    if guess in single_word:
+# No longer required in the function
+# if valid_guess:
+#     #convert guess into lower case   
+#     if guess.lower() in single_word:
+#         print(f"Good guess! {guess} is in the word.")
+#     else:
+#         print(f"Sorry {guess} is not in the word. Try again.")
+
+# Now adding 2 functions check_guess and ask_for_input for the 2 code blocks above
+# Step 2:
+# Convert the guess into lower case.
+# Step 3:
+# Move the code that you wrote to check if the guess is in the word into this function block.
+
+def check_guess(guess):
+    guess_lower = guess.lower()
+
+    if guess_lower in single_word:
         print(f"Good guess! {guess} is in the word.")
     else:
         print(f"Sorry {guess} is not in the word. Try again.")
 
+check_guess()
 
+# The ask_for_input function.
+def ask_for_input():
+    while True:
+        guess = input("Please guess the letter: ")
+        
+        if len(guess) == 1 and guess.isalpha():
+        # Added flag in order to move onto the next set of if statements, other was exiting due to break
+            valid_guess = True
+            break
+        else:
+            print("Invalid letter. Please enter a single alphabetical character.") 
 
+ask_for_input()
